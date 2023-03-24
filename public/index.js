e5da569f1b519f0498fe1130a5a63022a99c4e5f
Xiaoming <mengmengxiao@msn.com>
2023-03-24 22:37:03 +0800

    if the data string is '' or null then we need to remove the last prompt from the prompts array. if the prompts is too long then we need to remove the first prompt from the prompts array

--- public/index.js
+++ public/index.js
@@ -6,67 +6,83 @@ prompts.push(promptImpersonate);
 const messagesContainer = document.getElementById('messages');
 const messageForm = document.getElementById('message-form');
 const messageInput = document.getElementById('message-input');
+
+// 发送消息到API
 async function sendMessage(message) {
-  addMessage('user', message);
-  let userPrompt = "<|im_start|>user\n" + message + "\n<|im_end|>\n";
-  prompts.push(userPrompt);
-  const promptText = prompts.join('');
-  console.log(promptText);
+    addMessage('user', message);
+    let userPrompt = "<|im_start|>user\n" + message + "\n<|im_end|>\n";
+    prompts.push(userPrompt);
+    const promptText = prompts.join('');
+    console.log(promptText);
 
-  messageInput.value = '';
-  const response = await fetch('/api/gpt', {
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ prompt: promptText })
-  });
+    messageInput.value = '';
 
-  if (!response.ok) {
-    addMessage('bot', 'Error generating response.');
-    return;
-  }
+    // 发送请求到API
+    const response = await fetch('/api/gpt', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ prompt: promptText })
+    });
 
-  const data = await response.text();
-  console.log(data);
-  // 添加回复到prompt
-  prompts.push("<|im_start|>assistant\n" + data + "\n<|im_end|>\n");
-
-  addMessage('bot', data);
-}
-
-function addMessage(sender, message) {
-
-  const messageElement = document.createElement('div');
-  messageElement.classList.add('message');
-  messageElement.classList.add(`${sender}-message`);
-  if (message.includes('```')) {
-    const codeBlocks = message.match(/```(\w+)?\n([\s\S]+?)\n```/g);
-    if (codeBlocks) {
-      codeBlocks.forEach((block) => {
-        const code = block.replace(/```(\w+)?\n([\s\S]+?)\n```/, '$2');
-        const pre = document.createElement('pre');
-        const codeElement = document.createElement('code');
-        codeElement.innerText = code;
-        pre.appendChild(codeElement);
-        message = message.replace(block, pre.outerHTML);
-      });
+    if (!response.ok) {
+        addMessage('bot', 'Error generating response.');
+        return;
     }
-    messageElement.innerHTML = message;
-  } else {
-    messageElement.innerText = message;
-  }
 
-  messagesContainer.appendChild(messageElement);
-  messagesContainer.scrollTop = messagesContainer.scrollHeight;
+    let data = await response.text();
+    console.log(data);
+    //if the data string is '' or null, then we need to remove the last prompt from the prompts array
+    if (data === '' || data === null) {
+        prompts.pop();
+        data = "AI没有返回结果，请再说一下你的问题，或者换个问题问我吧。";
+    } else {
+        // 添加回复到prompt
+        prompts.push("<|im_start|>assistant\n" + data + "\n<|im_end|>\n");
+        //if the prompts is too long, then we need to remove the first prompt from the prompts array
+        if (prompts.length > 4) {
+            prompts.shift();
+        }
+        
+    }
+    addMessage('bot', data);
+    
 }
 
+// 添加消息到消息容器
+function addMessage(sender, message) {
+    const messageElement = document.createElement('div');
+    messageElement.classList.add('message');
+    messageElement.classList.add(`${sender}-message`);
+    if (message.includes('```')) {
+        const codeBlocks = message.match(/```(\w+)?\n([\s\S]+?)\n```/g);
+        if (codeBlocks) {
+            codeBlocks.forEach((block) => {
+                const code = block.replace(/```(\w+)?\n([\s\S]+?)\n```/, '$2');
+                const pre = document.createElement('pre');
+                const codeElement = document.createElement('code');
+                codeElement.innerText = code;
+                pre.appendChild(codeElement);
+                message = message.replace(block, pre.outerHTML);
+            });
+        }
+        messageElement.innerHTML = message;
+    } else {
+        messageElement.innerText = message;
+    }
+
+    messagesContainer.appendChild(messageElement);
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
+}
+
+// 发送消息
 messageForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const message = messageInput.value.trim();
-  if (message) {
-    sendMessage(message);
-  }
+    event.preventDefault();
+    const message = messageInput.value.trim();
+    if (message) {
+        sendMessage(message);
+    }
 });
 
 messageInput.focus();
