885efc3066a8c8ef80879666c000707cff16e2de
ming-macos <mengmengxiao@msn.com>
2024-04-13 21:43:03 +0800

    chore: Update webpack configuration to use webpack.DefinePlugin for environment variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const Dotenv = require("dotenv-webpack");
+const webpack = require("webpack"); // 引入 webpack
 
 module.exports = {
     mode: "development",
@@ -8,11 +8,18 @@ module.exports = {
         profileManager: "./public/profile-manager.js"
     },
     output: {
-        filename: "[name].bundle.js", // This will create main.bundle.js and profileManager.bundle.js
+        filename: "[name].bundle.js",
         path: path.resolve(__dirname, "./public/dist"),
     },
-    // 添加plugins字段，并实例化Dotenv
     plugins: [
-        new Dotenv()
+        new webpack.DefinePlugin({
+            "process.env.CLOUD_INSTANCE": JSON.stringify(process.env.CLOUD_INSTANCE),
+            "process.env.TENANT_ID": JSON.stringify(process.env.TENANT_ID),
+            "process.env.CLIENT_ID": JSON.stringify(process.env.CLIENT_ID),
+            "process.env.REDIRECT_URI": JSON.stringify(process.env.REDIRECT_URI),
+            "process.env.SCOPES": JSON.stringify(process.env.SCOPES),
+            "process.env.POST_LOGOUT_REDIRECT_URI": JSON.stringify(process.env.POST_LOGOUT_REDIRECT_URI),
+            "process.env.GRAPH_API_ENDPOINT": JSON.stringify(process.env.GRAPH_API_ENDPOINT),
+        }),
     ]
 };
