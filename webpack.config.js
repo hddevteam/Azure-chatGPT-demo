e1c5a6a9930ec401e515471042496a338812d349
ming-macos <mengmengxiao@msn.com>
2024-04-14 00:23:07 +0800

    chore: Update webpack configuration to use webpack.DefinePlugin for environment variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 const path = require("path");
-const webpack = require("webpack"); // 引入 webpack
+const webpack = require("webpack");
+const Dotenv = require("dotenv-webpack");
+
+// 检查是否在CI/CD环境中
+const isCICD = process.env.CI; // CI环境变量通常由CI/CD服务提供，例如GitHub Actions
 
 module.exports = {
     mode: "development",
@@ -12,14 +16,16 @@ module.exports = {
         path: path.resolve(__dirname, "./public/dist"),
     },
     plugins: [
-        new webpack.DefinePlugin({
-            "process.env.CLOUD_INSTANCE": JSON.stringify(process.env.CLOUD_INSTANCE),
-            "process.env.TENANT_ID": JSON.stringify(process.env.TENANT_ID),
-            "process.env.CLIENT_ID": JSON.stringify(process.env.CLIENT_ID),
-            "process.env.REDIRECT_URI": JSON.stringify(process.env.REDIRECT_URI),
-            "process.env.SCOPES": JSON.stringify(process.env.SCOPES),
-            "process.env.POST_LOGOUT_REDIRECT_URI": JSON.stringify(process.env.POST_LOGOUT_REDIRECT_URI),
-            "process.env.GRAPH_API_ENDPOINT": JSON.stringify(process.env.GRAPH_API_ENDPOINT),
-        }),
+        // 根据是否在CI/CD环境中使用不同的插件配置
+        ...(isCICD ? [
+            new webpack.DefinePlugin({
+                "process.env.CLOUD_INSTANCE": JSON.stringify(process.env.CLOUD_INSTANCE),
+                "process.env.TENANT_ID": JSON.stringify(process.env.TENANT_ID),
+                "process.env.CLIENT_ID": JSON.stringify(process.env.CLIENT_ID),
+                // 为其他需要的环境变量添加更多字段
+            }),
+        ] : [
+            new Dotenv()
+        ])
     ]
 };
